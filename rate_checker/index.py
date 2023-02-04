f539448dabaedffa6fa4da760e83133454b02aac
Sidney Ng <yendisng@gmail.com>
2023-02-04 16:24:26 -0500

    Update the rate checker script logic

--- rate_checker/index.py
+++ rate_checker/index.py
@@ -1,24 +1,26 @@
 import boto3
 import os
+from datetime import date
 
 ddb = boto3.client('dynamodb')
 table = os.environ['table']
+daily_limit = os.environ['daily_limit']
 
 
 def handler(event, context):
 
-    #     Daily: day, count
-    # Get Daily from DDB
-    # If count > 500
-    # 	return response to wait
-    # else return response to continue
-
     response = ddb.get_item(TableName=table, Key={
         'Type': {'S': 'daily'}
     })
+    day = response['Item']['Day']['S']
+    count = response['Item']['Count']['N']
 
-    print(response)
+    if count >= daily_limit:
+        count_date = date.fromisoformat(day)
+        print(count_date)
+        if count_date >= date.today():
+            return {'wait': True}
+        else:
+            print('reset count to 0')
 
-    return {
-        "success": True
-    }
+    return {'wait': False}
