08acc660b3d1848339ccf08c6a4beffbe6ee23b9
Sidney Ng <yendisng@gmail.com>
2023-02-11 16:36:29 -0500

    Update queueing logic to map a symbol to daytime to trigger data pull

--- queue_all_listings/index.py
+++ queue_all_listings/index.py
@@ -1,39 +1,65 @@
 import boto3
 import os
+from datetime import datetime, timedelta
 
 
 def handler(event, context):
     s3 = boto3.client('s3')
-    sqs = boto3.client('sqs')
     bucket = os.environ['bucket']
-    queue = os.environ['queue']
+    # Default will be 5
+    minute_limit = os.environ['minute_limit']
+    # Should be divisible by minute_limit. Default will be 500
+    day_limit = os.environ['day_limit']
 
     response = s3.get_object(Bucket=bucket, Key='listings.csv')
     byte_data = response['Body'].read()
     str_data = byte_data.decode('utf-8')
     rows_data = str_data.split('\n')
 
-    # Ignore the header row
-    for row_str in rows_data[1:]:
-        row = row_str.split(',')
-        symbol = row[0]
-        name = row[1]
-        if should_ignore_row(symbol, name):
-            print(f'ignoring symbol={symbol}, name={name}')
-        else:
-            print(f'queuing symbol={symbol}')
-            msg = '{"symbol": "%s"}' % (symbol)
-            response = sqs.get_queue_url(QueueName=queue)
-            queueUrl = response['QueueUrl']
-            sqs.send_message(QueueUrl=queueUrl, MessageBody=msg)
+    map_symbol_to_date(row_data=rows_data, daily_limit=day_limit,
+                       minute_limit=minute_limit)  # TODO: Handler
 
     return {
         "success": True
     }
 
 
-def should_ignore_row(symbol, name):
-    return '-' in symbol \
+def map_symbol_to_date(row_data, daily_limit, minute_limit, mapping_handler):
+    day_time = datetime.today() + timedelta(days=1)  # Start with the next day
+    symbols_per_day = daily_limit / minute_limit
+    day_count = 0
+    name_set = set()
+
+    # Ignore the header row
+    for row_str in row_data[1:]:
+        row = row_str.split(',')
+        symbol = row[0]
+        name = row[1]
+        type = row[3]
+        if should_ignore_row(symbol, name, type):
+            # print(f'ignoring symbol={symbol}, name={name}')
+            continue
+
+        unhyphenated_name = name.split(' - ')[0]
+        if unhyphenated_name in name_set:
+            # print(f'ignoring symbol={symbol}, name={name}')
+            continue
+        name_set.add(unhyphenated_name)
+
+        mapping_handler(symbol=symbol, day_time=day_time)
+
+        # Increment the minute and day count
+        day_count = day_count + 1
+        if day_count == symbols_per_day:
+            day_count = 0
+            day_time = day_time + timedelta(days=1)  # Next day
+        else:
+            day_time = day_time + timedelta(minutes=1)  # Next minute
+
+
+def should_ignore_row(symbol, name, type):
+    return type == 'ETF' \
+        or '-' in symbol \
         or '- Units' in name \
         or '- Warrants' in name \
         or 'Acquisition' in name \
--- queue_all_listings/test_ignore_row.py
+++ queue_all_listings/test_ignore_row.py
@@ -6,26 +6,32 @@ class TestDateComparison(unittest.TestCase):
 
     def test_should_ignore_row(self):
         self.assertEqual(should_ignore_row(
-            symbol='ZVV', name='LISTED TEST SYMBOL'
+            symbol='ZVV', name='LISTED TEST SYMBOL', type='STOCK'
         ), True)
         self.assertEqual(should_ignore_row(
             symbol='AACU',
-            name='Ares Corporation - Units (1 Ord Share Class A & 1/5 War)'
+            name='Ares Corporation - Units (1 Ord Share Class A & 1/5 War)',
+            type='STOCK'
         ), True)
         self.assertEqual(should_ignore_row(
             symbol='ACQRW',
-            name="Independence Holdings Corp - Warrants (31/03/2028)"
+            name="Independence Holdings Corp - Warrants (31/03/2028)",
+            type='STOCK'
         ), True)
         self.assertEqual(should_ignore_row(
-            symbol='AAC', name='Ares Acquisition Corporation - Class A'
+            symbol='AAC', name='Ares Acquisition Corporation - Class A',
+            type='STOCK'
         ), True)
         self.assertEqual(should_ignore_row(
-            symbol='AAC-U', name='Ares  Corporation'
+            symbol='AAC-U', name='Ares  Corporation', type='STOCK'
+        ), True)
+        self.assertEqual(should_ignore_row(
+            symbol='AACETF', name='An ETF', type='ETF'
         ), True)
 
     def test_should_not_ignore_row(self):
         self.assertEqual(should_ignore_row(
-            symbol='ACON', name='Aclarion Inc'
+            symbol='ACON', name='Aclarion Inc', type='STOCK'
         ), False)
 
 
