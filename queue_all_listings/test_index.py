746a3f4f63caf7e007cf0331b423851e465baa8d
Sidney Ng <yendisng@gmail.com>
2023-03-11 12:25:39 -0500

    Revert "Update queueing logic to map a symbol to daytime to trigger data pull"
    This reverts commit 08acc660b3d1848339ccf08c6a4beffbe6ee23b9.

--- queue_all_listings/test_index.py
+++ queue_all_listings/test_index.py
@@ -6,32 +6,26 @@ class TestDateComparison(unittest.TestCase):
 
     def test_should_ignore_row(self):
         self.assertEqual(should_ignore_row(
-            symbol='ZVV', name='LISTED TEST SYMBOL', type='STOCK'
+            symbol='ZVV', name='LISTED TEST SYMBOL'
         ), True)
         self.assertEqual(should_ignore_row(
             symbol='AACU',
-            name='Ares Corporation - Units (1 Ord Share Class A & 1/5 War)',
-            type='STOCK'
+            name='Ares Corporation - Units (1 Ord Share Class A & 1/5 War)'
         ), True)
         self.assertEqual(should_ignore_row(
             symbol='ACQRW',
-            name="Independence Holdings Corp - Warrants (31/03/2028)",
-            type='STOCK'
+            name="Independence Holdings Corp - Warrants (31/03/2028)"
         ), True)
         self.assertEqual(should_ignore_row(
-            symbol='AAC', name='Ares Acquisition Corporation - Class A',
-            type='STOCK'
+            symbol='AAC', name='Ares Acquisition Corporation - Class A'
         ), True)
         self.assertEqual(should_ignore_row(
-            symbol='AAC-U', name='Ares  Corporation', type='STOCK'
-        ), True)
-        self.assertEqual(should_ignore_row(
-            symbol='AACETF', name='An ETF', type='ETF'
+            symbol='AAC-U', name='Ares  Corporation'
         ), True)
 
     def test_should_not_ignore_row(self):
         self.assertEqual(should_ignore_row(
-            symbol='ACON', name='Aclarion Inc', type='STOCK'
+            symbol='ACON', name='Aclarion Inc'
         ), False)
 
 
