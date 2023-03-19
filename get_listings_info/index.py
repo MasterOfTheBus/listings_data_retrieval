38a7f1db3dede2f9fab4644ab9acc6b685f99cf0
Sidney Ng <yendisng@gmail.com>
2023-03-19 16:02:29 -0400

    update get listings to pull from queue

--- get_listings_info/index.py
+++ get_listings_info/index.py
@@ -3,9 +3,11 @@ import requests
 import os
 
 s3 = boto3.client('s3')
+sqs = boto3.client('sqs')
 ddb = boto3.client('dynamodb')
 bucket = os.environ['bucket']
 table = os.environ['table']
+retrieval_table = os.environ['retrievalTable']
 api_key = os.environ['apiKey']
 base_url = 'https://www.alphavantage.co'
 query_types = [
@@ -15,9 +17,18 @@ query_types = [
 
 def handler(event, context):
 
+    queue_url = get_queue_info(ddb, sqs, retrieval_table)
+    response = sqs.receive_message(QueueUrl=queue_url,
+                                   WaitTimeSeconds=20)
+    messages = response['Messages']
+    if len(messages) == 0:
+        return {'queue_empty': True}
+    message = response['Messages'][0]
+    symbol = message['Body']['symbol']
+
     for type in query_types:
         try:
-            get_query_type_and_store(type=type, listing=event['listing'])
+            get_query_type_and_store(type=type, listing=symbol)
         except RuntimeError:
             raise
 
@@ -27,8 +38,17 @@ def handler(event, context):
                     UpdateExpression='SET #C=#C+:count')
 
     return {
-        "success": True
+        "queue_empty": False
     }
+
+
+def get_queue_info(ddb, sqs, table):
+    recv_q_item = ddb.get_item(TableName=table, Key={
+        'queue': {'S': 'receive_queue'}})
+    recv_q_name = recv_q_item['Item']['name']['S']
+    print(f'Queue is {recv_q_name}')
+    response = sqs.get_queue_url(QueueName=recv_q_name)
+    return response['QueueUrl']
 
 
 def get_query_type_and_store(type, listing):
