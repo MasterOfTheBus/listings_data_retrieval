0ebf4c56d78f2aa934dbe04006fb0a07eb1dc52d
Sidney Ng <yendisng@gmail.com>
2023-04-01 13:13:45 -0400

    switch to storing the next symbol in ddb

--- get_listings_info/index.py
+++ get_listings_info/index.py
@@ -2,56 +2,44 @@ import boto3
 import requests
 import os
 
-s3 = boto3.client('s3')
-sqs = boto3.client('sqs')
-ddb = boto3.client('dynamodb')
-bucket = os.environ['bucket']
-table = os.environ['table']
-retrieval_table = os.environ['retrievalTable']
-api_key = os.environ['apiKey']
-base_url = 'https://www.alphavantage.co'
-query_types = [
-    'OVERVIEW', 'INCOME_STATEMENT', 'BALANCE_SHEET', 'CASH_FLOW', 'EARNINGS'
-]
-
 
 def handler(event, context):
-
-    queue_url = get_queue_info(ddb, sqs, retrieval_table)
-    response = sqs.receive_message(QueueUrl=queue_url,
-                                   WaitTimeSeconds=20)
-    messages = response['Messages']
-    if len(messages) == 0:
-        return {'queue_empty': True}
-    message = response['Messages'][0]
-    symbol = message['Body']['symbol']
+    s3 = boto3.client('s3')
+    ddb = boto3.client('dynamodb')
+    bucket = os.environ['bucket']
+    rate_table = os.environ['rateTable']
+    symbols_table = os.environ['symbolsTable']
+    api_key = os.environ['apiKey']
+    base_url = 'https://www.alphavantage.co'
+    query_types = [
+        'OVERVIEW', 'INCOME_STATEMENT', 'BALANCE_SHEET',
+        'CASH_FLOW', 'EARNINGS'
+    ]
+    symbol = event['symbol']
 
     for type in query_types:
         try:
-            get_query_type_and_store(type=type, listing=symbol)
+            get_query_type_and_store(s3, bucket, base_url,
+                                     api_key, type, symbol)
         except RuntimeError:
             raise
 
-    ddb.update_item(TableName=table, Key={'Type': {'S': 'daily'}},
-                    ExpressionAttributeNames={'#C': 'Count'},
-                    ExpressionAttributeValues={':count': {'N': '5'}},
-                    UpdateExpression='SET #C=#C+:count')
+    update_rate_limit(ddb, rate_table)
 
     return {
-        "queue_empty": False
+        'queue_empty': False,
+        'symbol': get_next_symbol(ddb, symbols_table, symbol)
     }
 
 
-def get_queue_info(ddb, sqs, table):
-    recv_q_item = ddb.get_item(TableName=table, Key={
-        'queue': {'S': 'receive_queue'}})
-    recv_q_name = recv_q_item['Item']['name']['S']
-    print(f'Queue is {recv_q_name}')
-    response = sqs.get_queue_url(QueueName=recv_q_name)
-    return response['QueueUrl']
+def get_next_symbol(ddb, table, current_symbol):
+    response = ddb.get_item(TableName=table, Key={
+        'symbol': {'S': current_symbol}
+    })
+    return response['Item']['next']['S']
 
 
-def get_query_type_and_store(type, listing):
+def get_query_type_and_store(s3, bucket, base_url, api_key, type, listing):
     URL = f'{base_url}/query?function={type}&symbol={listing}&apikey={api_key}'
     with requests.Session() as s:
         download = s.get(URL)
@@ -61,3 +49,10 @@ def get_query_type_and_store(type, listing):
 
         s3.put_object(
             Bucket=bucket, Key=f'{listing}/{type}.json', Body=download.content)
+
+
+def update_rate_limit(ddb, rate_table):
+    ddb.update_item(TableName=rate_table, Key={'Type': {'S': 'daily'}},
+                    ExpressionAttributeNames={'#C': 'Count'},
+                    ExpressionAttributeValues={':count': {'N': '5'}},
+                    UpdateExpression='SET #C=#C+:count')
