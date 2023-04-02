4c9b1ee14cf85db822e93c6b3e071602ac48a720
Sidney Ng <yendisng@gmail.com>
2023-04-02 10:22:11 -0400

    Return next day to wait until for rate checker

--- rate_checker/index.py
+++ rate_checker/index.py
@@ -1,6 +1,6 @@
 import boto3
 import os
-from datetime import date
+from datetime import date, time, datetime, timedelta
 
 
 def handler(event, context):
@@ -18,7 +18,8 @@ def handler(event, context):
 
     if count >= daily_limit:
         if current_day_before_or_equals_saved_date(day):
-            return {'wait': True, 'symbol': next_symbol}
+            next_day = calc_wait_until_next_day(date.today())
+            return {'wait': True, 'symbol': next_symbol, 'next_day': next_day}
         else:
             today = date.today().isoformat()
             ddb.update_item(TableName=table, Key={'Type': {'S': 'daily'}},
@@ -34,6 +35,12 @@ def handler(event, context):
             print(f'reset count to 0, day to {today}')
 
     return {'wait': False, 'symbol': next_symbol}
+
+
+def calc_wait_until_next_day(current_date):
+    noon_time_local = time(hour=12)
+    next_day = current_date + timedelta(days=1)
+    return datetime.combine(date=next_day, time=noon_time_local)
 
 
 def current_day_before_or_equals_saved_date(saved_date_str):
--- rate_checker/test_rate_checker.py
+++ rate_checker/test_rate_checker.py
@@ -1,9 +1,12 @@
 import unittest
-from datetime import date, timedelta
-from index import current_day_before_or_equals_saved_date
+from datetime import date, timedelta, datetime
+from index import current_day_before_or_equals_saved_date, \
+    calc_wait_until_next_day
 
 
 class TestDateComparison(unittest.TestCase):
+
+    # current_day_before_or_equals_saved_date Tests
 
     def test_current_day_before_stored_date(self):
         day = date.today() + timedelta(days=10)
@@ -19,6 +22,16 @@ class TestDateComparison(unittest.TestCase):
         day = date.today()
         result = current_day_before_or_equals_saved_date(day.isoformat())
         self.assertEqual(result, True)
+
+    # calc_wait_until_next_day Tests
+
+    def test_calc_wait_until_next_day(self):
+        test_day = date(year=2023, month=4, day=1)
+        expected_day = datetime(year=2023, month=4, day=2,
+                                hour=12, minute=0, second=0)
+
+        next_day = calc_wait_until_next_day(test_day)
+        self.assertEqual(next_day, expected_day)
 
 
 if __name__ == '__main__':
